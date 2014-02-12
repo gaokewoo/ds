3797ae83031a4695ec9a034e3179f50d2949b54e
Gaoke Wu <gaokewoo@126.com>
2014-02-12 20:29:03 +0800

    [task] add programe to gen negtive or positive data

--- test.cc
+++ test.cc
@@ -7,24 +7,28 @@
 
 using namespace std;
 
-int main()
+const int DATA_NUM = 100000;
+const int NEG_POS_DATA_NUM = 30;
+
+void genGneralData()
 {
-    const int DATA_NUM = 100000;
-    //ofstream f("./data.txt");
+    ofstream f("./data.txt");
 
-    //srand( (unsigned)time( NULL ) ); 
-    //if(f.good())
-    //{
-    //    for(int i=0;i<DATA_NUM;i++)
-    //    {
-    //        int j = rand()%1000;
-    //        f<<j<<" ";
-    //    }
-    //}
+    srand( (unsigned)time( NULL ) ); 
+    if(f.good())
+    {
+        for(int i=0;i<DATA_NUM;i++)
+        {
+            int j = rand()%1000;
+            f<<j<<" ";
+        }
+    }
 
-    //f.close();
+    f.close();
+}
 
-
+void testGeneralData()
+{
     int *arr=new int[DATA_NUM];
     ifstream in_file;
     in_file.open("./data.txt",ios::in);
@@ -40,6 +44,54 @@ int main()
     st.doSort(arr, DATA_NUM);
 
     delete[] arr;
+
+}
+
+void genNegPosData()
+{
+    ofstream f("./data2.txt");
+
+    srand( (unsigned)time( NULL ) ); 
+    if(f.good())
+    {
+        for(int i=0;i<NEG_POS_DATA_NUM;i++)
+        {
+            int j = rand()%1000;
+            if(rand()%10%2==0) j=-j;
+            f<<j<<" ";
+        }
+    }
+
+    f.close();
+
+}
+
+void testNegPosData()
+{
+    int *arr=new int[NEG_POS_DATA_NUM];
+    ifstream in_file;
+    in_file.open("./data2.txt",ios::in);
+    for(int k=0; !in_file.eof() && k<NEG_POS_DATA_NUM; k++)
+    {
+        int j;
+        in_file>>j;
+        arr[k]=j;
+    }
+    in_file.close();
+
+    Sort st;
+    st.doSort(arr, NEG_POS_DATA_NUM);
+
+    delete[] arr;
+}
+
+int main()
+{
+    genGneralData();
+    testGeneralData();
+
+    genNegPosData();
+    testNegPosData();
 
     return 0;
 }
