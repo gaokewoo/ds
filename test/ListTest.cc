e8b9ce66bcbd823c85e29e828e3e8b1923a93b51
Gaoke Wu <gaokewoo@126.com>
2014-02-21 22:29:35 +0800

    [task] Migrate list test case to google test framework

--- test/ListTest.cc
+++ test/ListTest.cc
@@ -1,27 +1,68 @@
 #include <iostream>
+#include <fstream>
 #include "gtest/gtest.h"
+#include "alg/List.hh"
 
 using namespace std;
 
-class ListTest:public testing::Test{
+class ListTest:public testing::Test
+{
     protected:
 
-        virtual void SetUp(){
-            s="abc";
+        virtual void SetUp()
+        {
+            arr = new int[DATA_NUM];
+            ifstream in_file;
+            in_file.open("../data/data.txt",ios::in);
+            for(int k=0; !in_file.eof() && k<DATA_NUM; k++)
+            {
+                int j;
+                in_file>>j;
+                arr[k]=j;
+            }
+            in_file.close();
         }
 
-        string s;
+        virtual void TearDown()
+        {
+            delete [] arr;
+            arr=NULL;
+        }
+
+
+    protected:
+        enum {DATA_NUM = 100000};
+        int * arr;
 };
 
-TEST_F(ListTest,assign) {
-	string s2("abc",2);
-	EXPECT_EQ(s2,"ab");
+TEST_F(ListTest,assign) 
+{
+    List<int> my_list;
 
-	string s3(s,2); //attention:this function is different with the previous function
-	EXPECT_EQ(s3,"c");
+    for(int k=0; k<DATA_NUM; k++)
+    {
+        my_list.insert(arr[k]);
+    }
 
-	string s4(s,2,2);
-	EXPECT_EQ(s4,"c");
+    int sum=0,k=DATA_NUM-1;
+    Node<int> *p = my_list.getFirst();
+    bool not_equal=false;
+    while(p!=NULL)
+    {
+        sum++; //compute element number
+        if(arr[k]!=my_list.retrieve(p)) //compare the list value equal the arr value
+        {
+            not_equal=true;
+            break;
+        }
 
+        my_list.del(my_list.retrieve(p));
+        p=my_list.getNext(p);
+        k--;
+    }
+
+    ASSERT_TRUE(not_equal == false);
+
+    EXPECT_EQ(DATA_NUM, sum);
 }
 
