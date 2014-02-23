085ebfa857b6d3b169eeeb2c25d7a9e48d5d454e
Gaoke Wu <gaokewoo@126.com>
2014-02-23 23:15:53 +0800

    [task] Add implementation of stack via list

--- alg/Stack.hh
+++ alg/Stack.hh
@@ -5,90 +5,128 @@ using namespace std;
 namespace StackList
 {
 
-template <typename T> class Stack;
+    template <typename T> class Stack;
 
-template<typename T>
-class Node
-{
-    friend class Stack<T>;
-    T elem;
-    Node *next;
-};
+    template<typename T>
+        class Node
+        {
+            friend class Stack<T>;
+            T elem;
+            Node *next;
+        };
 
-template<typename T>
-class Stack
-{
-    public:
-        Stack();
-        ~Stack();
-        bool isEmpty();
-        void makeEmpty();
-        void push(T x);
-        T top();
-        void pop();
+    template<typename T>
+        class Stack
+        {
+            public:
+                Stack();
+                ~Stack();
+                bool isEmpty();
+                void makeEmpty();
+                void push(T x);
+                T top();
+                T pop();
 
-    private:
-        Node<T> *s;
-};
+            private:
+                Node<T> *s;
+        };
 
-template<typename T>
-Stack<T>::Stack()try:s(new Node<T>)
-{
-    s->next=NULL;
-}
-catch(bad_alloc &e)
-{
-    cerr<<"Stack alloc Node error."<<endl;
-    cerr<<"Error info:"<<e.what()<<endl;
-}
-
-template<typename T>
-Stack<T>::~Stack()
-{
-    if(!isEmpty())
-        makeEmpty();
-
-    try
-    {
-        delete s;
-        s=NULL;
-    }
+    template<typename T>
+        Stack<T>::Stack()try:s(new Node<T>)
+        {
+            s->next=NULL;
+        }
     catch(bad_alloc &e)
     {
-        cerr<<"Stack release Node error."<<endl;
+        cerr<<"Stack alloc Node error."<<endl;
         cerr<<"Error info:"<<e.what()<<endl;
     }
-}
 
-template<typename T>
-bool Stack<T>::isEmpty()
-{
-    cout<<"I'm empty"<<endl;
-    return true;
-}
+    template<typename T>
+        bool Stack<T>::isEmpty()
+        {
+            return s->next == NULL;
+        }
 
-template<typename T>
-void Stack<T>::makeEmpty()
-{
+    template<typename T>
+        T Stack<T>::pop()
+        {
+            try
+            {
+                T e;
+                Node<T> *p = s->next;
+                s->next = p->next;
+                if(p != NULL)
+                {
+                    e = p->elem;
+                    delete p;
 
-}
+                    return e;
+                }
+                else
+                {
+                    cerr<<"Stack is empty."<<endl;
 
-template<typename T>
-void Stack<T>::push(T x)
-{
+                    throw;
+                }
+            }
+            catch(bad_alloc &e)
+            {
+                cerr<<"Release Node error."<<endl;
+                cerr<<"Error info:"<<e.what()<<endl;
+            }
 
-}
+        }
 
-template<typename T>
-T Stack<T>::top()
-{
-    return T(0);
-}
+    template<typename T>
+        void Stack<T>::makeEmpty()
+        {
+            while(!isEmpty())
+            {
+                pop();
+            }
+        }
 
-template<typename T>
-void Stack<T>::pop()
-{
+    template<typename T>
+        Stack<T>::~Stack()
+        {
+            if(!isEmpty())
+              makeEmpty();
 
-}
+            try
+            {
+                delete s;
+                s=NULL;
+            }
+            catch(bad_alloc &e)
+            {
+                cerr<<"Stack release Node error."<<endl;
+                cerr<<"Error info:"<<e.what()<<endl;
+            }
+        }
 
-}
+    template<typename T>
+        void Stack<T>::push(T x)
+        {
+            try
+            {
+                Node<T> *p = new Node<T>;
+                p->elem = x;
+                p->next = s->next;
+                s->next = p;
+            }
+            catch(bad_alloc & e)
+            {
+                cerr<<"Alloc Node error."<<endl;
+                cerr<<"Error info:"<<e.what()<<endl;
+            }
+
+        }
+
+    template<typename T>
+        T Stack<T>::top()
+        {
+            return s->next->elem;
+        }
+
+};
---
+++ test/StackTest.cc
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <fstream>
+#include "gtest/gtest.h"
+#include "alg/Stack.hh"
+
+using namespace std;
+
+class StackTest:public testing::Test
+{
+    protected:
+
+        virtual void SetUp()
+        {
+            arr = new int[DATA_NUM];
+            ifstream in_file;
+            in_file.open("../data/data.txt",ios::in);
+            for(int k=0; !in_file.eof() && k<DATA_NUM; k++)
+            {
+                int j;
+                in_file>>j;
+                arr[k]=j;
+            }
+            in_file.close();
+        }
+
+        virtual void TearDown()
+        {
+            delete [] arr;
+            arr=NULL;
+        }
+
+
+    protected:
+        enum {DATA_NUM = 100000};
+        int * arr;
+};
+
+TEST_F(StackTest,testStackList) 
+{
+    using namespace StackList;
+
+    Stack<int> my_stack;
+
+    for(int k=0; k<DATA_NUM; k++)
+    {
+        my_stack.push(arr[k]);
+    }
+
+    int sum=0,k=DATA_NUM-1;
+    bool not_equal=false;
+    while(!my_stack.isEmpty())
+    {
+        sum++; //compute element number
+        if(arr[k]!=my_stack.pop()) //compare the list value equal the arr value
+        {
+            not_equal=true;
+            break;
+        }
+
+        k--;
+    }
+
+    ASSERT_TRUE(not_equal == false);
+
+    EXPECT_EQ(DATA_NUM, sum);
+}
+
